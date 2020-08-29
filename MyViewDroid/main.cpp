62286ff4b1a59d6a14f1c4d549cfa97e68f28d62
v02qabs <takesue090@gmail.com>
2020-08-30 01:11:31 +0900

    move-japan-language

--- MyViewDroid/main.cpp
+++ MyViewDroid/main.cpp
@@ -5,13 +5,14 @@
 #include <QtWebKitWidgets/QWebView>
 #include <QHBoxLayout>
 #include <QUrl>
+#include <QLineEdit>
 int main(int argc, char *argv[])
 {
 		if(argc < 2)
 		{
 						qDebug() << "error";
 		}
-		else
+		els
 		{
 						qDebug() << argv[1];
 		}
@@ -20,25 +21,30 @@ int main(int argc, char *argv[])
 
     QApplication a(argc, argv);
     QWidget *myActivity = new QWidget();
-    myActivity->setWindowTitle("HelloGUi");
+    myActivity->setWindowTitle("MyViewer");
     //Qtlayout
-    QHBoxLayout *layout = new QHBoxLayout;
-		//command
-/*    QPushButton *push_OK = new QPushButton("OK", myActivity);
-		QLineEdit *url_my = new QLineEdit;
-		command->addWidget(url_my);
-		command->addWidget(push_ok);
-		
-	
-		
-
-*/
+    QHBoxLayout *v_layout = new QHBoxLayout;
+    QHBoxLayout *h_layout = new QHBoxLayout;
+    QVBoxLayout *main_layout = new QVBoxLayout;
+		//h_layout
+    QPushButton *push_ok = new QPushButton("GO", myActivity);
+    QLineEdit *editor = new QLineEdit(myActivity);
+   // editor.setS
+    v_layout->addWidget(push_ok);
+    v_layout->addWidget(editor);
+		//QLineEdit *url_my = new QLineEdit;
+		//h_layout->addLayout(v_layout);
+		//h_layout->addWidget(push_ok);
 		//webview
 		QWebView *web_view = new QWebView();
+		
 		web_view -> load(QUrl(address));
-    //layout->addWidget(push_OK);
-    layout->addWidget(web_view);
-    myActivity->setLayout(layout);
-    myActivity->show();
+		
+    //h_layout -> addLayout(v_layout);
+    h_layout->addWidget(web_view);
+    main_layout -> addLayout(v_layout);
+    main_layout -> addLayout(h_layout);
+    myActivity -> setLayout(main_layout);
+   myActivity->show();
     return a.exec();
 }
