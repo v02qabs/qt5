7cc20afd1a21c79b76e656c7f8b96dd6e124732a
v02qabs <takesue090@gmail.com>
2020-12-26 07:18:26 +0900

    sqlite3-db-test-Db

---
+++ c++/qtsql/Main.cpp
@@ -0,0 +1,35 @@
+#include <QtSql/QSqlDatabase>
+#include <QtSql/QSqlQuery>
+#include <QDebug>
+#include <QCoreApplication>
+
+
+int main(int argc , char  *argv[])
+{
+	QSqlDatabase db;
+	    db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName("testDB");
+    if(!db.open())
+    {
+        qDebug() << "Can't Connect to DB !";
+    }
+    else
+    {
+        qDebug() << "Connected Successfully to DB !";
+        QSqlQuery query;
+        query.prepare("SELECT coutry FROM user");
+        if(!query.exec())
+        {
+            qDebug() << "Can't Execute Query !";
+        }
+        else
+        {
+            qDebug() << "Query Executed Successfully !";
+            while(query.next())
+            {
+                qDebug() << "country Name : " << query.value(0).toString();
+            }
+        }
+    }
+}
+
